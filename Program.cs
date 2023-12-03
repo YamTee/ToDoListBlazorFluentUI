439705dfc3d4282f36a1ebe5c98a2a9b372a042e
Mohammed Thalha <mohammed.t@ibridgellc.com>
2023-12-03 22:03:46 +0530

    Added Local storage servide, and accesing todo from local storage

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using ToDoListBlazorFluentUI;
+using ToDoListBlazorFluentUI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
 
 builder.Services.AddScoped(sp =>
         new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ Services/LocalStorageService.cs
@@ -0,0 +1,64 @@
+﻿namespace ToDoListBlazorFluentUI.Services;
+
+public interface ILocalStorageService
+{
+    Task<T?> GetValueAsync<T>(string key);
+    Task RemoveAsync(string key);
+    Task SetValueAsync<T>(string key, T value);
+    Task Clear();
+}
+
+public class LocalStorageService(IJSRuntime jsRuntime) : IAsyncDisposable, ILocalStorageService
+{
+    private Lazy<IJSObjectReference> _accessorJsRef = new();
+    private readonly IJSRuntime _jsRuntime = jsRuntime;
+
+    private async Task WaitForReference()
+    {
+        if (!_accessorJsRef.IsValueCreated)
+        {
+            _accessorJsRef = new(await _jsRuntime
+                .InvokeAsync<IJSObjectReference>("import", "./js/localStorage.js"));
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_accessorJsRef.IsValueCreated)
+        {
+            await _accessorJsRef.Value.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public async Task<T?> GetValueAsync<T>(string key)
+    {
+        await WaitForReference();
+        var value = await _accessorJsRef.Value.InvokeAsync<string>("get", key);
+
+        var result = JsonConvert.DeserializeObject<T>(value);
+
+        return result;
+    }
+
+    public async Task SetValueAsync<T>(string key, T value)
+    {
+        await WaitForReference();
+
+        var jsonValue = JsonConvert.SerializeObject(value);
+
+        await _accessorJsRef.Value.InvokeVoidAsync("set", key, jsonValue);
+    }
+
+    public async Task Clear()
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("clear");
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("remove", key);
+    }
+}
